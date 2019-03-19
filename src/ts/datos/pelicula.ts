00106a57f92e4abd76bf39439f6264b598229cdb
Javier <jbenito.dz@gmail.com>
2019-03-20 00:54:17 +0100

    Added Pelicula class

--- src/ts/datos/pelicula.ts
+++ src/ts/datos/pelicula.ts
@@ -1,1 +1,40 @@
 /* Aquí deberás crear la clase para instanciar cada una de las películas */
+import * as moment from "moment";
+import { PeliculaJSON } from "./peliculaJSON";
+
+type Valoracion = 0 | 1 | 2 | 3 | 5;
+enum Formato {
+  DVD,
+  VHS,
+  archivo
+}
+
+class Pelicula {
+  public label: string;
+  public id: number;
+  public titulo: string;
+  public director: string;
+  public cartel: string;
+  public vista: false;
+  public formato: Formato;
+  public oscars: number;
+  public valoracion: Valoracion;
+  private fecha: moment.Moment;
+
+  constructor(pelicula: PeliculaJSON) {
+    this.label = pelicula.label;
+    this.id = pelicula.id;
+    this.titulo = pelicula.titulo;
+    this.director = pelicula.director;
+    this.fecha = moment(pelicula.fecha, "DD-MM-YYYY");
+    this.cartel = pelicula.cartel;
+    this.vista = pelicula.vista;
+    this.formato = Formato[pelicula.formato];
+    this.oscars = pelicula.oscars;
+    this.valoracion = pelicula.valoracion as Valoracion;
+  }
+
+  public getFormato = (): string => Formato(this.formato);
+
+  public getYear = (): string => this.fecha.year().toString();
+}
---
+++ src/ts/datos/peliculaJSON.ts
@@ -0,0 +1,12 @@
+export interface PeliculaJSON {
+  label: string;
+  id: number;
+  titulo: string;
+  director: string;
+  fecha: string;
+  cartel: string;
+  vista: false;
+  formato: string;
+  oscars: number;
+  valoracion: number;
+}
