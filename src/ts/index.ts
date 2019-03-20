d5ad8fa807cd61de29ad4c46c283d327c048c3cf
Javier <jbenito.dz@gmail.com>
2019-03-21 00:05:10 +0100

    Initiate vista-peliculas and added delete lists methods

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,7 @@
 import { ControladorPeliculas } from './controlador-peliculas';
+import { VistaPeliculas } from './vista-peliculas';
 
 const peliculasCtrl = new ControladorPeliculas();
 peliculasCtrl.loadPeliculas();
+
+const peliculasView = new VistaPeliculas();
--- src/ts/vista-peliculas.ts
+++ src/ts/vista-peliculas.ts
@@ -1,1 +1,32 @@
-/* Aquí deberás crear la clase para el controlador de la vista, que será el que se comunique directamente con el HTML */
+enum Category { Vista, Pendiente }
+
+export class VistaPeliculas {
+  private HTML: any = {}; // FIXME: Tipar???
+
+  constructor() {
+    this .loadHTML();
+    this .cleanPeliculasList(Category.Pendiente);
+    this .cleanPeliculasList(Category.Vista);
+  }
+
+  /**
+   * Load HTML elements
+   */
+  private loadHTML(): void {
+    this .HTML.jsListaPendientes = document.querySelector('.js-lista-pendientes');
+    this .HTML.peliculasPendientes = this .HTML.jsListaPendientes.querySelectorAll('li');
+    this .HTML.jsListaVistas = document.querySelector('.js-lista-vistas');
+    this .HTML.peliculasVistas = this .HTML.jsListaVistas.querySelectorAll('li');
+  }
+
+  /**
+   * Remove peliculas list
+   * @param category of the films (enum Category)
+   */
+  private cleanPeliculasList(category: Category): void {
+    const itemList = this .HTML[`peliculas${Category[category]}s`];
+    for (const item of itemList) {
+      item.remove();
+    }
+  }
+}
